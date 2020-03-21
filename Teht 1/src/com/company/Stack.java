176a9d0a3cc579c41c521aa6534f2ff1362653c4
Benjamin Bowo <ben.bowo@gmail.com>
2020-03-21 15:45:54 +0200

    Teht 1 Push

--- Teht 1/src/com/company/Stack.java
+++ Teht 1/src/com/company/Stack.java
@@ -1,30 +1,52 @@
 package com.company;
 
+import java.util.*;
+
 public class Stack {
     
     private ListItem top;
     private int size;
+    LinkedList<ListItem> list = new LinkedList<>();
 
+    /**********************************************************/
     
-    // muodosta uusi lista-alkio ja vie se pinon huipulle
+    // Muodosta uusi lista-alkio ja vie se pinon huipulle
     public void push(String aData){
         top = new ListItem();
-
+        top.setData(aData);
+        list.add(top);
+        size++;
     }
 
-    // poista ja palauta alkio pinon huipulta, 
-    // jos pino tyhjä palauta null
+    /**********************************************************/
+
+    // Poista ja palauta alkio pinon huipulta, jos pino tyhjä palauta null
     public ListItem pop(){
-        
-        return null;
+        if (size == 0) {
+            return null;
+        }
+        top = list.get(size - 1);
+        list.removeLast();
+        size--;
+        return top;
     }
-    // tulosta pinon sisältö muuttamatta pinoa
+
+    /**********************************************************/
+
+    // Tulosta pinon sisältö muuttamatta pinoa
     public void printItems() {
-        
+        if (size == 0) {
+            System.out.println("Tyhjä pino");
+        }
+
+       for (ListItem item : list) {
+           System.out.println(item.getData());
+        }
     }
+
+    /**********************************************************/
 
     public int getSize() {
         return size;
     }
-    
 }
